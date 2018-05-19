0ef9cda8206db37118b31ece8d537a6de4312171
kalmanbencze <kalmanbncz@gmail.com>
2018-05-19 17:22:21 +0300

    created the edit dog layout

--- app/src/main/java/me/kalmanbncz/doggydaycare/presentation/browse/edit/EditFragment.java
+++ app/src/main/java/me/kalmanbncz/doggydaycare/presentation/browse/edit/EditFragment.java
@@ -6,16 +6,20 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
+import java.util.List;
 import javax.inject.Inject;
 import me.kalmanbncz.doggydaycare.R;
 import me.kalmanbncz.doggydaycare.presentation.BaseFragment;
@@ -53,6 +57,35 @@ public class EditFragment extends BaseFragment {
     public void onPause() {
         unbindViewModel();
         super.onPause();
+    }
+
+    public void setAdapterForSpinner(AppCompatSpinner spinner, List<String> items, String hint) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item) {
+
+            @Override
+            public int getCount() {
+                return super.getCount() - 1; // you dont display last item. It is used as hint.
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                View v = super.getView(position, convertView, parent);
+                if (position == getCount()) {
+                    ((TextView) v.findViewById(android.R.id.text1)).setText("");
+                    ((TextView) v.findViewById(android.R.id.text1)).setHint(getItem(getCount())); //"Hint to be displayed"
+                }
+
+                return v;
+            }
+        };
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.addAll(items);
+        adapter.add(hint);
+
+        spinner.setAdapter(adapter);
+        spinner.setSelection(adapter.getCount()); //display hint
     }
 
     private void unbindViewModel() {
